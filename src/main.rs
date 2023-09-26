0d0270abfbaafb687f9c27d6ac23feb0a6275294
Alpi Tolvanen <alpi.tolvanen@atostek.com>
2023-09-26 18:00:38 +0300

    Fix wording

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@
 /// 
 /// You can run individual test cases with 
 ///     cargo test <test case name>
-/// Or you can run all test cases that begin with common prefix
+/// Or you can run all test cases that begin with common prefixs
 ///     cargo test <common prefix>
 /// For example, to run all tests of the problem 2:
 ///     cargo test test_problem_2
 /// 
-/// Instead of running unit tests, you can also do experiments by calling the code from
-/// the main()-function. Then you can run it with a command
+/// Instead of running unit tests, you can also call the code from
+/// the main()-function. In that case you can run it with a command
 ///     cargo run
 /// This is useful for debug-prints, because tests suppress the output.
 
